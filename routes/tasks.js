3087867be5ba7feff26348519d61b0149cecaccb
WEREMSOFT <pablo.weremczuk@gmail.com>
2016-05-30 09:51:52 -0300

    added automatic gulp task that reloads the server when something changess

--- routes/tasks.js
+++ routes/tasks.js
@@ -4,12 +4,12 @@ var router = express.Router();
 
 router.get('/api/tasks', function(req, res){
 	res.json([
-		{title: 'my Task', startTime: '9:00', endTime:'12:00', comment: 'this is odd'},
-		{title: 'my Task', startTime: '9:00', endTime:'12:00', comment: 'this is odd'},
-		{title: 'my Task', startTime: '9:00', endTime:'12:00', comment: 'this is odd'},
-		{title: 'my Task', startTime: '9:00', endTime:'12:00', comment: 'this is odd'},
-		{title: 'my Task', startTime: '9:00', endTime:'12:00', comment: 'this is odd'},
-		{title: 'my Task', startTime: '9:00', endTime:'12:00', comment: 'this is odd'},
+		{title: 'Wash Dogs', startTime: '9:00', endTime:'12:00', comment: 'this is odd'},
+		{title: 'Clean House', startTime: '9:00', endTime:'12:00', comment: 'this is odd'},
+		{title: 'Make Food', startTime: '9:00', endTime:'12:00', comment: 'this is odd'},
+		{title: 'See TV', startTime: '9:00', endTime:'12:00', comment: 'this is odd'},
+		{title: 'Help Homework', startTime: '9:00', endTime:'12:00', comment: 'this is odd'},
+		{title: 'Finish Him', startTime: '9:00', endTime:'12:01', comment: 'this is odd'}
 	]);
 });
 
