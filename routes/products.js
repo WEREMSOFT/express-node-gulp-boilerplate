59b27a9b3ceecf39e58da828edb011fa19e7749d
WEREMSOFT <pablo.weremczuk@gmail.com>
2016-05-31 14:23:41 -0300

    added celery call to products

--- routes/products.js
+++ routes/products.js
@@ -1,10 +1,20 @@
 var express = require('express');
+var request = require('request');
 var router = express.Router();
 
 
-router.get('/api/products', function(req, res){
-	res.json({
-		response: 'Hello Products!'
+router.get('/api/products', function (req, res) {
+	var headers = {
+		'Content-Type': 'application/json',
+		'Authorization': process.env.CELERY_ACCESS_TOKEN
+	};
+	var options = {
+		url: 'https://api.trycelery.com/v2/products',
+		path: '/',
+		headers: headers
+	};
+	request(options, function(error, response, body){
+		res.json(JSON.parse(response.body));
 	});
 });
 
