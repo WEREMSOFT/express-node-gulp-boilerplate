f7671f4c9ce043f9493192e1618fe6733376c69e
WEREMSOFT <pablo.weremczuk@gmail.com>
2016-05-31 14:10:54 -0300

    Added request for proxy example

--- routes/products.js
+++ routes/products.js
@@ -1,10 +1,18 @@
 var express = require('express');
+var request = require('request');
 var router = express.Router();
 
 
-router.get('/api/products', function(req, res){
-	res.json({
-		response: 'Hello Products!'
+router.get('/api/products', function (req, res) {
+	var headers = {
+		'Content-Type': 'text/html; charset=utf-8',
+	};
+	var options = {
+		url: 'https://ar.search.yahoo.com/search?p=cadorna&fr=yfp-t-726',
+		headers: headers
+	};
+	request(options, function(error, response, body){
+		res.send(response.body);
 	});
 });
 
